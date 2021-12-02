f0ebac023ea033679e3b60b61409746a9597887d
Hui Guo <huiguo@fb.com>
2021-12-02 12:32:38 -0800

    [tensorexpr] Add BufLiveRange to obtain the liveness range of a buf in a block stmt
    ghstack-source-id: c975a5d1e92bd817b05ec5dace12d5810ff9bfb3
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/66450

--- torch/csrc/jit/tensorexpr/analysis.h
+++ torch/csrc/jit/tensorexpr/analysis.h
@@ -248,6 +248,110 @@ class ModifiesVarChecker : public IRVisitor {
   bool found_{false};
 };
 
+// Traverse the Block stmt to identify the live range of the specified buf. The
+// live range, indicated by a pair of integers, specifies the first and last
+// stmt in block stmts that access to the buf.
+class BufLiveRange : public IRVisitor {
+ public:
+  BufLiveRange(BufPtr b) : buf_(b) {}
+
+  std::tuple<int32_t, int32_t> getLiveRange() {
+    return std::make_tuple(begin_, end_);
+  }
+
+  static std::tuple<int32_t, int32_t> liveRange(StmtPtr s, BufPtr b) {
+    BlockPtr block = to<Block>(s);
+    TORCH_INTERNAL_ASSERT(
+        block, "Only analze buffer live range for block stmt");
+    BufLiveRange analyzer(b);
+    block->accept(&analyzer);
+    return analyzer.getLiveRange();
+  }
+
+ private:
+  bool findBufReads(StmtPtr s) {
+    auto loads1 = NodeFinder<Load>::find(s);
+    for (auto l : loads1) {
+      if (l->buf() == buf_) {
+        return true;
+      }
+    }
+    auto loads2 = NodeFinder<ExternalCall>::find(s);
+    for (auto l : loads2) {
+      for (auto lb : l->buf_args()) {
+        if (lb == buf_) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  bool findBufWrites(StmtPtr s) {
+    auto writes1 = NodeFinder<Store>::find(s);
+    for (auto w : writes1) {
+      if (w->buf() == buf_) {
+        return true;
+      }
+    }
+    auto writes2 = NodeFinder<ExternalCall>::find(s);
+    for (auto w : writes2) {
+      if (w->buf() == buf_) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  void updateRange(StmtPtr s) {
+    bool has_reads = findBufReads(s), has_writes = findBufWrites(s);
+    if (has_reads || has_writes) {
+      if (begin_ == -1) {
+        begin_ = curr_index_;
+      };
+      end_ = curr_index_;
+    }
+  }
+
+  void visit(StorePtr v) {
+    updateRange(v);
+  }
+
+  void visit(LetPtr v) {
+    updateRange(v);
+  }
+
+  void visit(AtomicAddPtr v) {
+    updateRange(v);
+  }
+
+  void visit(ExternalCallPtr v) {
+    updateRange(v);
+  }
+
+  void visit(BlockPtr v) {
+    // Stmt counter of the block
+    bool enable_count = false;
+    for (StmtPtr s : *v) {
+      if (to_count_) {
+        curr_index_ += 1;
+        to_count_ = false;
+        enable_count = true;
+      }
+      s->accept(this);
+      if (enable_count) {
+        to_count_ = true;
+      }
+    }
+  }
+
+  BufPtr buf_;
+  int32_t begin_ = -1;
+  int32_t end_ = -1;
+  int32_t curr_index_ = -1;
+  bool to_count_ = true;
+};
+
 // A class that analyzes the given program relevant for Block backend
 // It creates a map of multi dim buffers and their flat verions
 class CreateBufferMap : public IRVisitor {
